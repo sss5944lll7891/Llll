91f8b70f6220cc7333ec73cd22f8e7a991285a76
sss5944lll7891 <uu9364461@gmail.com>
2026-09-25 21:06:53 +0800

    Add files via upload

---
+++ V3.0 手机 - 副本.lua
@@ -0,0 +1,2 @@
+有人反馈代码过长无法复制到忍者注入器所以说上传到了github，如果你想要获取源码可以单独把下面脚本github网站截下来然后开启魔法复制到浏览器打开获取源码【没有加密】
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1215203698741/Roblox-ESP-Antibot-V3/refs/heads/main/V3.0phone.lua"))()
